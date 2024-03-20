5b5678c0f18f81b8b97595fdf59594fc9d586555
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-20 10:19:00 +0100

    chore: add tailwind, update style

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 interface ButtonProps {
-    onClick: () => void;
-    children: React.ReactNode;
+  onClick: () => void;
+  children: React.ReactNode;
 }
 
 export const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button
+      className="rounded-lg border py-2 px-4 text-base font-medium bg-slate-950 cursor-pointer transition-colors hover:border-cyan-600 focus:outline-4 focus-visible:outline-4"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
